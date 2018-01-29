9aa3da90dadb72cc72dd3b8fa6510c67c976ead1
Estevan Seyfried <estevanseyfried@gmail.com>
2018-01-28 21:49:32 -0800

    Switch to BrowserRouter and start routing.

---
+++ src/components/ProjectFeed.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ProjectFeed extends React.Component{
+	render(){
+		return(
+			<h1>This is the Project Feed</h1>
+		);
+	}
+}
+
+export default ProjectFeed;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Switch, Route, Redirect} from 'react-router-dom'
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
 import database from './database';
 
 import App from './components/App';
+import ProjectFeed from './components/ProjectFeed';
+import LogIn from './components/LogIn';
 
 class Root extends React.Component{
 	constructor(){
 		super()
 		//This binding is necesary to make `this` work
+		//this.database = this.database.bind(this);
 		this.state={}
 	}
 	render(){
 		return(
-			<HashRouter>
+			<BrowserRouter>
 				<App>
 					<Switch>
+						<Route exact path='/' component={ProjectFeed}/>
+						<Route exact path='/login' component={LogIn}/>
 					</Switch>
 				</App>
-			</HashRouter>
+			</BrowserRouter>
 		)
 	}
 }
