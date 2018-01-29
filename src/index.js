c92e4276bf92c89275147b35d2a6c29ff00a74f1
Estevan Seyfried <estevanseyfried@gmail.com>
2018-01-28 19:57:16 -0800

    Converting App to Firebase & File structure fixes.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import {HashRouter, Switch, Route, Redirect} from 'react-router-dom'
+import database from './database';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+
+class Root extends React.Component{
+	constructor(){
+		super()
+		//This binding is necesary to make `this` work
+		this.state={}
+	}
+	render(){
+		return(
+			<HashRouter>
+				<App>
+					<Switch>
+					</Switch>
+				</App>
+			</HashRouter>
+		)
+	}
+}
+
+ReactDOM.render(
+	<Root/>,document.getElementById('root'))
