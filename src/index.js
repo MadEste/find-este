ce24abdfe113d594389f516fa1136e63c36704cb
Estevan Seyfried <estevanseyfried@gmail.com>
2018-01-29 20:19:24 -0800

    basic Auth Works

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
-import database from './database';
+import {db, firebaseAuth} from './database';
+
 
 import App from './components/App';
 import ProjectFeed from './components/ProjectFeed';
@@ -11,9 +12,30 @@ class Root extends React.Component{
 	constructor(){
 		super()
 		//This binding is necesary to make `this` work
-		//this.database = this.database.bind(this);
+		//this.database = database.bind(this);
 		this.state={}
 	}
+	componentDidMount(){
+		var provider = new firebaseAuth.GithubAuthProvider();
+		firebaseAuth().signInWithPopup(provider).then(function(result) {
+		  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+		  var token = result.credential.accessToken;
+		  // The signed-in user info.
+		  var user = result.user;
+		  // ...
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // The email of the user's account used.
+		  var email = error.email;
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+		  // ...
+		});
+	}
+	//TODO get figure out where to store popup code for GitHubOauth through firebase
+	// Pass Database object to 
 	render(){
 		return(
 			<BrowserRouter>
