b5e475f0d9265b2967d94e5a4f6f58850ae35b03
Estevan Seyfried <estevanseyfried@gmail.com>
2018-02-03 14:44:54 -0800

    Splashpage and local storage Created

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './css//App.css';
+import './css/App.css';
+
+import NavBar from './Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <NavBar/>
         <div id="main">
           {this.props.children}
         </div>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
-import {db, firebaseAuth} from './database';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {firebaseAuth} from './database';
 
 /*
 	TODO
-		 - setup redirect on gitPopUp and logout
+		- Start DEFINING PROPTYPES IN COMPONENTS
 		 - define what is needed for Project model
 		 - change what is viewed depending if logged in or not
 		  - set fields for default template.
 */
 
 import App from './components/App';
-import ProjectFeed from './components/ProjectFeed';
 import LogIn from './components/LogIn';
+import ProjectFeed from './components/ProjectFeed';
+import Splash from './components/Splash';
 
 class Root extends React.Component{
 	constructor(){
@@ -21,8 +22,10 @@ class Root extends React.Component{
 		//This binding is necesary to make `this` work in the call back
 		this.gitPopUp = this.gitPopUp.bind(this);
 		this.gitLogOut = this.gitLogOut.bind(this);
+		this.updateVisit = this.updateVisit.bind(this);
 		this.state={
-			user:null
+			user:null,
+			iVisit:true
 		}
 	}
 	componentWillMount(){
@@ -30,25 +33,37 @@ class Root extends React.Component{
 		firebaseAuth().onAuthStateChanged( user => {
 		  this.setState({user});
 		});
+		//Check Local Storage for inital visits
+		try{
+			let jsonVisited = localStorage.getItem('iVisit');
+			if(jsonVisited !== null){
+				//exists pull it into state
+				let iVisit = JSON.parse(jsonVisited);
+				this.setState({iVisit});
+			}
+		}catch(err){
+      console.log(err);
+    }
 	}
 	gitPopUp(history){
 		let provider = new firebaseAuth.GithubAuthProvider();
 		firebaseAuth().signInWithPopup(provider).then( (result) => {
 		  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-		  var token = result.credential.accessToken;
+		  //var token = result.credential.accessToken;
 		  // The signed-in user info.
-		  var user = result.user;
+		  //var user = result.user;
 		  // ...ADD REDIRECT
 		  history.push('/');
 		}).catch( error => {
 		  // Handle Errors here.
-		  var errorCode = error.code;
-		  var errorMessage = error.message;
+		  let errorCode = error.code;
+		  let errorMessage = error.message;
 		  // The email of the user's account used.
-		  var email = error.email;
+		  //var email = error.email;
 		  // The firebase.auth.AuthCredential type that was used.
-		  var credential = error.credential;
+		  //var credential = error.credential;
 		  // ...
+		  console.log(errorCode+" , "+errorMessage);
 		});
 	}
 	gitLogOut(history){
@@ -61,12 +76,31 @@ class Root extends React.Component{
 		  console.log(error);
 		});
 	}
+	updateVisit(){
+		//save to local storage in a try/catch Block
+		try {
+		  localStorage.setItem('iVisit', 'false');
+		} catch (err) {
+		  // Ignore write errors.
+		  console.log(err);
+		}
+		//update state
+		this.setState({
+			iVisit:false
+		});
+	}
 	render(){
 		return(
 			<BrowserRouter>
 				<App>
 					<Switch>
-						<Route exact path='/' component={ProjectFeed}/>
+						<Route exact path='/' render={() => {
+							if(this.state.iVisit){
+								return(<Splash updateVisit={this.updateVisit}/>);
+							}else{
+								return(<ProjectFeed/>);
+							}
+						}}/>
 						<Route exact path='/login' render={ ({history}) => {
 							return <LogIn history={history} gitPopUp={this.gitPopUp} gitLogOut={this.gitLogOut} user={this.state.user}/>;
 						}}/>
